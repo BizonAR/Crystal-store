381f4f4d4436d3578ff2d04a79e3bdee84f7f18a
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 00:34:55 +0500

    Crystal store

---
+++ CrystalStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalStore
+{
+	internal class CrystalStore
+	{
+		static void Main(string[] args)
+		{
+			int priceCrystal = 10;
+
+			Console.Write("Сколько у вас золота? ");
+			int gold = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Сколько кристаллов вы хотите купить? ");
+			int numberCrystals = Convert.ToInt32(Console.ReadLine());
+			gold -= numberCrystals * priceCrystal;
+
+			Console.WriteLine($"У вас осталось золота: {gold} и вы купили {numberCrystals} кристаллов");
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CrystalStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrystalStore")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("248432bf-f354-408d-b551-de2a82a88be6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
